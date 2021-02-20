861a1aa13d0506a6098dc151d756a844852e434a
Sachin Joshi 12687 <sachinjoshi12687@gmail.com>
2021-02-20 23:26:36 +0530

    Update WebDriverManager_Sample.py

--- WebDriverManager_Sample.py
+++ WebDriverManager_Sample.py
@@ -3,10 +3,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
-driver.implicitly_wait(5)
-driver.get("https://www.amazon.in/")
-print(driver.title)
-time.sleep(5)
-driver.quit()
+class mytest():
+  def testbasic(self):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.implicitly_wait(5)
+    driver.get("https://www.amazon.in/")
+    print(driver.title)
+    time.sleep(5)
+    driver.quit()
