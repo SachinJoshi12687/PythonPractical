a35829dd0a8e6c860c8c95abefd0c149b1bfa17d
Sachin Joshi 12687 <sachinjoshi12687@gmail.com>
2022-04-28 10:11:21 +0530

    Update printvariable.py

--- printvariable.py
+++ printvariable.py
@@ -1,6 +1,6 @@
 def test():
     print("====================")
-    print("Hello this is my test output line")
+    print("Hello this is my test output line1")
     print("====================")
 
 
